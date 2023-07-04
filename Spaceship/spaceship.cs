ad1d5bf1280ff23dfb0f046adc2f1cc625c6de4e
mattley <matvejperepelkin@gmail.com>
2023-07-04 06:35:12 +0000

    create project and tests

---
+++ Spaceship/spaceship.cs
@@ -0,0 +1,24 @@
+﻿name Spaceship;
+public class Spaceship
+{
+public static double[] Movement(double[] ShipCoordinates,double[] ShipWay, bool CanMove ){
+
+        if (CanMove == false) {
+            throw new System.Exception();
+        }
+        
+        foreach(double x in ShipCoordinates.Concat(ShipWay).ToArray()){
+            if (double.IsNaN(x) | double.IsInfinity(x)){
+             throw new System.Exception();
+            }    
+        }
+        
+
+        for (int i = 0; i < ShipCoordinates.Length;i++){
+            ShipCoordinates[i] += ShipWay[i];
+        }
+
+        return  ShipCoordinates;
+        }
+
+}
