ec29fe857d8d427aace516be1de56181ea38ef1e
mattley <matvejperepelkin@gmail.com>
2023-07-04 18:16:52 +0000

    added fuel and rotation

--- Spaceship.Tests/spaceship.tests.cs
+++ Spaceship.Tests/spaceship.tests.cs
@@ -63,3 +63,101 @@ public class Spaceship_Tests
         Assert.ThrowsAsync<Exception>(() => throw actualException);
     }
 }
+[Binding, Scope(Feature = "Движение при достаточном количестве топлива")]
+    public class StepDefinitions2
+{
+    private double fuelBalance = double.NaN;
+    private double fuelNeed = double.NaN;
+    private Exception actualException = new Exception();
+    private double actualResult = double.NaN; 
+
+	[Given(@"^космический корабль имеет топливо в объеме (.*) ед")]
+    public void КоординатыКорабляВТочке(double fuelAm)
+    { 
+        fuelBalance = fuelAm; 
+    }
+
+    [Given(@"^имеет скорость расхода топлива при движении (.*) ед")]
+    public void СкоростьКорабля(double fuelCons)
+    { 
+        fuelNeed = fuelCons;  
+    }
+
+    [When(@"^происходит прямолинейное равномерное движение без деформации")]
+    public void ДвижениеКорабля(){
+        try{
+            actualResult = Spaceship.UseFuel(fuelBalance, fuelNeed);
+        }
+        catch(Exception e){
+            actualException = e;
+        }
+    }
+
+    [Then(@"новый объем топлива космического корабля равен (.*) ед")]
+    public void КосмическийКорабльПеремещаетсяВТочкуПространства(double expectedResult)
+    {
+        Assert.Equal(expectedResult, actualResult, 6);
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ВозникаетОшибка()
+    {
+        Assert.ThrowsAsync<Exception>(() => throw actualException);
+    }
+}
+
+[Binding, Scope(Feature = "Разворот")]
+public class StepDefinitions3
+{
+    private double instant = double.NaN;
+    private double rotationalSpeed = double.NaN;
+    private double actualResult = double.NaN;
+    private bool rotation = true;
+    private Exception actualException = new Exception();
+
+	[Given(@"^космический корабль имеет угол наклона (.*) град к оси OX")]
+    public void УголНаклона(double incl)
+    { 
+        instant = incl;
+    }
+
+    [Given(@"^космический корабль, угол наклона которого невозможно определить")]
+    public void УголНаклонаОпределитьНевозможно(){}
+
+    [Given(@"^имеет мгновенную угловую скорость (.*) град")]
+    public void УгловаяСкорость(double rotSpeed)
+    { 
+        rotationalSpeed = rotSpeed;
+    }
+
+    [Given(@"^мгновенную угловую скорость невозможно определить")]
+    public void СкоростьВращения(){}
+
+    [Given(@"невозможно изменить уголд наклона к оси OX космического корабля")]
+         public void НевозможноИзменитьУголдНаклонаКОсиOXКосмическогоКорабля()
+         {
+            rotation = false;
+         }
+
+    [When(@"^происходит вращение вокруг собственной оси")]
+    public void Вращение(){
+        try{
+            actualResult = Spaceship.Rotation(instant, rotationalSpeed, rotation);
+        }
+        catch(Exception e){
+            actualException = e;
+        }
+    }
+
+    [Then(@"угол наклона космического корабля к оси OX составляет (.*) град")]
+    public void УголНаклонаСтановится(double expectedResult)
+    {
+        Assert.Equal(expectedResult, actualResult, 6);
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ВозникаетОшибка()
+    {
+        Assert.ThrowsAsync<Exception>(() => throw actualException);
+    }
+}
--- Spaceship/spaceship.cs
+++ Spaceship/spaceship.cs
@@ -1,16 +1,11 @@
 ﻿namespace Spaceship;
 public class Spaceship
 {
-public static double[] Movement(double[] ShipCoordinates,double[] Direction, bool CanMove ){
+    public static double[] Movement(double[] ShipCoordinates,double[] Direction, bool CanMove ){
 
-        if (CanMove == false) {
-            throw new System.Exception();
-        }
-        
-        if (ShipCoordinates.Any(double.IsNaN) || Direction.Any(double.IsInfinity)){
+        if (ShipCoordinates.Any(double.IsNaN) || Direction.Any(double.IsInfinity)|| CanMove == false){
          throw new System.Exception(); 
         }
-        
 
         for (int i = 0; i < ShipCoordinates.Length;i++){
             ShipCoordinates[i] += Direction[i];
@@ -18,5 +13,20 @@ public static double[] Movement(double[] ShipCoordinates,double[] Direction, boo
 
         return  ShipCoordinates;
         }
+
+    public static double UseFuel(double fuelBalance ,double fuelNeed){
+        if(fuelNeed>fuelBalance){
+            throw new System.Exception();
+        }
+        return fuelBalance-fuelNeed;
+    }
+
+    public static double Rotation (double degX,double instantSpeed,bool rotate){
+    double[] currency = new double[]{degX,instantSpeed};
+    if (currency.Any(double.IsNaN) || currency.Any(double.IsInfinity)|| rotate == false)
+        throw new System.Exception(); 
+    
+    return degX + instantSpeed;
+    }
 
 }
